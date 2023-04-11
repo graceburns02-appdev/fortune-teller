b0f62d649c91d7366532254e5238387485230f00
Grace Burns <graceburns@chicagobooth.edu>
2023-04-11 14:12:11 +0000

    lucky and unlucky done

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,21 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @losers = Array.new
+
+    5.times do
+      unlucky = rand(1...100)
+      @losers.push(unlucky)
+    end
+
+    render({ :template => "lottery_stuff/unlucky.html.erb" })
+  end
 end
